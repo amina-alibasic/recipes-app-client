283694361e2510377d0baaf843fb863371b359f5
Amina Alibasic <alibasicamina@outlook.com>
2023-04-18 21:51:28 +0200

    Implementing searchbar

--- src/components/searchbar/searchbar.component.ts
+++ src/components/searchbar/searchbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
@@ -9,15 +9,20 @@ import {map, startWith} from 'rxjs/operators';
   styleUrls: ['./searchbar.component.css']
 })
 export class SearchbarComponent  implements OnInit {
+
+  @Input()  placeholder: string = '';
+
+
   myControl = new FormControl('');
   options: string[] = ['One', 'Two', 'Three'];
   filteredOptions!: Observable<string[]>;
-
+searchbarPlaceholder: string = '';
   ngOnInit() {
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
       map(value => this._filter(value || '')),
     );
+    this.searchbarPlaceholder = 'Search for ' + this.placeholder + '...'
   }
 
   private _filter(value: string): string[] {
@@ -25,4 +30,5 @@ export class SearchbarComponent  implements OnInit {
 
     return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
+
 }
