9c5bbef5346c24886d2e30d30245882ffe5e1936
Amina Alibasic <alibasicamina@outlook.com>
2024-11-02 20:44:01 +0100

    Font edits

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  goToHome() {
+    const currentRoute = window.location.pathname;
+    if (currentRoute !== '/home') {
+      window.location.href = '/home';
+    }
+  }
 }
